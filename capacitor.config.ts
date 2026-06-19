e7c0d2a587915ab7dde3022081b58051cde6c6fd
cfngc4594 <cfngc4594@gmail.com>
2026-06-19 14:16:02 +0800

    Add Capacitor support with configuration and dependencies; update .gitignore and package.json

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.massbug.bead",
+  appName: "Bead",
+  webDir: "out",
+};
+
+export default config;
